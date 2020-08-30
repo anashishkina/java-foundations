4906bc1b392aa19f6d3f5e8a8ab507c6a312a984
anashishkina <anaschishkina@gmail.com>
2020-08-30 21:37:01 +0200

    innitial commit

--- src/main/java/object/hw/Flat.java
+++ src/main/java/object/hw/Flat.java
@@ -6,5 +6,8 @@ public class Flat {
 
         Person anton = new Person("Anton", 25, 180);
         System.out.println("anton.toString() = " + anton.toString());
+
+        Person elena = new Person("Elena", 18, 175);
+        System.out.println("elena.toString() = " + elena.toString());
     }
 }
